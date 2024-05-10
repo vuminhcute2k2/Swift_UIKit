a0715ccf8bc767e73e3edef99802cea82e659265
vuminhcute2k2 <minhvu69696969@gmail.com>
2024-05-10 14:50:24 +0700

    Initial Commit

---
+++ autoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  autoLayout
+//
+//  Created by Minh Vũ on 10/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
